ac0cb80dd42218cc132a4bed1e5ae88f0044eafc
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-19 23:09:02 +0300

    Uygulama:Değişken tanımlama

---
+++ variablesdemo.py
@@ -0,0 +1,40 @@
+"""
+Uygulama 1 :Bir öğrencinin aşağıdaki bilgileri için değişkenler oluşturunuz.
+Öğrenci adı
+Öğrenci soyadı
+Öğrenci ad+soyad
+Öğrenci numarası
+Öğrenci cinsiyet
+Öğrenci tc kimlik
+Öğrenci doğum yılı
+Öğrenci adres bilgisi
+Öğrenci yaşı
+"""
+#Uygulama 1 Çözümü
+ogrenciAdi="Hüseyin"
+ogrenciSoyadi="Tunç"
+ogrenciAdiSoyadi=ogrenciAdi+" "+ogrenciSoyadi
+ogrenciNumarasi=123456
+ogrenciCinsiyet="Erkek"
+ogrenciTcKimlik="12345678901"
+ogrenciDogumYili=2003
+ogrenciAdres="Gümüşhane"
+ogrenciYasi=21
+print("{} {} {}".format(ogrenciAdi, ogrenciSoyadi, ogrenciAdiSoyadi))
+
+
+
+"""
+Uygulama 2:Aşağıdaki ürünlerin toplam bilgisini hesaplayınız.
+Ürün 1 adet fiyatı 50 TL
+Ürün 2 adet fiyatı 60 TL
+Ürün 3 adet fiyatı 70 TL
+"""
+#Uygulama 2 Çözümü
+urun1AdetFiyati=50
+urun2AdetFiyati=60
+urun3AdetFiyati=70
+toplam=urun1AdetFiyati+urun2AdetFiyati+urun3AdetFiyati
+print(toplam)
+
+
