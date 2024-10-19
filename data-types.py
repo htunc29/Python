8f94e548c5539620784f10a1530064cb39bd8fbe
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-19 23:17:19 +0300

    Pythonda Veri Tipleri

---
+++ data-types.py
@@ -0,0 +1,20 @@
+#int
+#float
+#string
+#bool
+"""
+
+x = int(input("x: "))
+y = int(input("y: "))
+print(type(x))
+print(type(y))
+toplam=x+y
+print(toplam)
+"""
+x=5
+y=2.5
+name="Hüseyin"
+isStudent=True
+
+x=float(x)
+print(x)
