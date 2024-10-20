1f782fcba794e9391c2eaa0004c8d2ff7c19658f
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-20 10:43:36 +0300

    Pythonda Listeler

---
+++ Pythonda Listeler/lists.py
@@ -0,0 +1,10 @@
+msg = "Merhaba ben bir mesajım".split()
+print(msg[0])
+print(msg[1][:4])
+sayilar=[1,3,5,7,9]
+sonuc=sayilar
+sonuc=sayilar[3]
+ogrenciler=[["Ali",12],["Ahmet",22]]
+sonuc=ogrenciler[0][0]
+sonuc=ogrenciler[1][0]
+print(sonuc)
