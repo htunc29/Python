bef654941093cf659a8ca264634d29680a2c5904
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-20 10:26:18 +0300

    Uygulama:Karakter Dizileri:Strings

---
+++ Python Strings/strings_demo.py
@@ -0,0 +1,12 @@
+website="http://www.vekaletim.com"
+kursAdi="Python Dersleri: Baştan Sona Python Programlama"
+print(len(kursAdi))
+print(website[7:10])
+print(website[-4:])
+print(website[:15])
+print(website[-15:])
+print(website[::-1])
+print(website.replace("w","W"))
+
+name,surname,age,job="Hüseyin","Tunç",21,"Öğrenci"
+print(f"Benim adım {name} {surname}, yaşım {age} ve mesleğim {job}.")
