118593731ad9cf4174ea3852276344a98d032e17
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 19:34:04 +0300

    Pythonda For Döngüleri

---
+++ Pythonda Döngüler/for.py
@@ -0,0 +1,17 @@
+sayılar=[1,2,3,6,8,9]
+isimler=["ali","deniz","yağmur"]
+# for x in sayılar:
+#     print(x)
+# for x in sayılar:
+#     print("Merhaba")
+# for x in isimler:
+#     print(x)
+# students={
+#     2311081001:"Hüseyin Tunç",
+#     2311081002:"Ali Deneme"
+# }
+# for x,y in students.items():
+#     print(x,y)
+degerler=[(1,2),(3,5)]
+for x,y in degerler:
+    print(x,y)
