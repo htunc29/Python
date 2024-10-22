038631c723e5cdb68599989ae551b64b4569a73b
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 11:52:40 +0300

    Pythonda Listeler 2

---
+++ Pythonda Listeler/lists-2.py
@@ -0,0 +1,12 @@
+diller=["Python","C#","Java","C++","Javascript"]
+sonuc=diller
+sonuc=diller
+diller[-1]="HTML"
+newdil=diller+["CSS"]
+if "Python" in diller:
+    print("Evet")
+print(sonuc)
+print(len(sonuc))
+print(newdil)
+for x in diller:
+    print(x)
