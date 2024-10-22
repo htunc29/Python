d9b87234c1b715e87def73603a297a424990e7ef
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 20:37:47 +0300

    Döngü metotları:enumerate(),zip()

---
+++ Pythonda Döngüler/enumerate-zip.py
@@ -0,0 +1,20 @@
+markalar=["opel","bmw","mercedes"]
+
+# index=1
+# for marka in markalar:
+#     print(f"{index}-{marka}")
+#     index+=1
+
+# obj1=enumerate(markalar)
+
+# for x,y in obj1:
+#     print(x,y)
+
+# liste1=[1,2,3,4,5]
+# liste2=['a','b','c','d','e']
+
+
+# obj2=list(zip(liste1,liste2))
+# for x,y in obj2:
+#     print(x,y)
+
