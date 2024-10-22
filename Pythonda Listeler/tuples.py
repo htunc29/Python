4d87e728c1a6ed6202b058476cde7339bbb316c9
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 12:28:28 +0300

    Pythonda Tuple

---
+++ Pythonda Listeler/tuples.py
@@ -0,0 +1,11 @@
+_list=[1,2,3]
+_tuple=(1,2,3,"iki","kırmızı")
+_tuple2=(1,2,3,"iki","kırmızı")
+
+print(type(_list))
+print(type(_tuple))
+print(_tuple[2])
+print(_list)
+print(_tuple+_tuple2)
+_t=tuple([3,4,5])
+print(_t)
