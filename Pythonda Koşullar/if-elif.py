6c9368ff7d466a04eeb08ed7508dbaa3cdc7020e
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 18:16:05 +0300

    Koşullu Durum Blokları - If - Elif - Else Blokları

---
+++ Pythonda Koşullar/if-elif.py
@@ -0,0 +1,19 @@
+# x=10
+# y=4
+
+
+# if x>y:
+#     print("x y den büyük")
+# elif x==y:
+#     print("x eşittir y")
+# else:
+#     print("x y den küçük")
+
+sayi=float(input("Number:"))
+
+if(sayi<0):
+    print("Number is negative")
+elif(sayi==0):
+    print("Number is equals zero")
+else:
+    print("Number is positive")
