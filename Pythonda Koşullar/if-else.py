8f4efaed79d213723004167c5f7ea58cc4f6ce19
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 18:12:35 +0300

    Koşullu Durum Blokları - If ve Else Blokları

---
+++ Pythonda Koşullar/if-else.py
@@ -0,0 +1,10 @@
+username=input("Username:")
+password=input("Password:")
+
+if(username=="tunc29"):
+    if(password==1234):
+        print("Login Success")
+    else:
+        print("Password is not correct")
+else:
+    print("User Not Found")
