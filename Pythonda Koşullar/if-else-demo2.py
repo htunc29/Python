f7c8c96aae0bc2093455c3a9f22760ba50edb7b1
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-22 18:41:06 +0300

    Uygulama 2:Yakıt tüketim maliyeti

---
+++ Pythonda Koşullar/if-else-demo2.py
@@ -0,0 +1,21 @@
+#Bir aracın yakıt tipine göre (benzin,dizel) belirtilen bir mesafede ne kadar
+#yakıt masrafı olduğunu hesaplayan uygulamayı yapınız.
+benzin=40.34
+dizel=45.21
+
+
+fueltype=input("What is Fuel Type your car ?(benzin/dizel):").lower()
+
+unitfuel=float(input("How much use fuel your car in one hundred kilometer ?: "))
+
+km=int(input("How many kilometers ? :"))
+
+total=unitfuel*(km/100)
+if(fueltype=="benzin"):
+    print(f"{km} yolda bu arac {int(total*benzin)} ₺ yakıt tüketir")
+else:
+    print(f"{km} yolda bu arac {int(total*dizel)} ₺ yakıt tüketir")
+
+
+
+
