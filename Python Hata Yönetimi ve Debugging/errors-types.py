8e9fa9fdfa88ad784706cd944d72754d1e585d94
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-26 11:45:12 +0300

    Hata türleri - Exception Types

---
+++ Python Hata Yönetimi ve Debugging/errors-types.py
@@ -0,0 +1,10 @@
+# x=int(input("x: "))
+# y=int(input("y: "))
+# print(x/y)
+# SyntaxError =>yazım yanlışı
+# NameError   =>tanımlanmamış bir değişken kullanımı
+# TypeError   =>hatalı parametre kullanımı
+# IndexError  =>yanlış indeks numarası
+# ValueError  =>hatalı tip kullanımı
+# KeyError    =>key hatası
+# AttributeError =>olmayan bir özelliğe ulaşmak
