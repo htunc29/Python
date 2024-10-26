badb3452792e9f5140e3285808966f5069dced0c
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-26 12:10:34 +0300

    Modül Kullanımı

---
+++ Pythonda Modüller/Modül/main.py
@@ -0,0 +1,9 @@
+import modul
+sonuc=modul.sayi
+print(sonuc)
+sonuc=modul.topla(1,2)
+print(sonuc)
+from modul import ogrenci
+print(ogrenci["numara"])
+from modul import *
+print(sayilar)
---
+++ Pythonda Modüller/Modül/modul.py
@@ -0,0 +1,9 @@
+sayi=10
+sayilar=[10,20,30]
+ogrenci={
+    "ad":"ali",
+    "numara":10,
+    "notlar":[70,80,90]
+}
+def topla(a,b):
+    return a+b
