c074c344252489e52863b09cffd6d25bd96e83e6
Hüseyin Tunç <huseyint428@gmail.com>
2024-10-28 12:24:57 +0300

    Child sınıfıların genişletilmesi

--- Nesne Tabanlı Programlama/abstract.py
+++ Nesne Tabanlı Programlama/abstract.py
@@ -6,19 +6,33 @@ class Person:
     def intro(self):
         print(self.name,self.surname,self.age)
 class Student(Person):
-    pass
+    def __init__(self,name,surname,age,number):
+        super().__init__(name,surname,age)
+        self.number=number
+        print("Student nesnesi üretildi")
+    def study(self):
+        print(f"{self.number} numaralı öğrenci ders çalışıyor")
+    def intro(self):
+        print(self.name,self.surname,self.age)
 class Teacher(Person):
-    pass
+    def __init__(self,name,surname,age,branch):
+        super().__init__(name,surname,age)
+        self.branch=branch
+        print("Teacher nesnesi üretildi")
+    def teach(self):
+        print(f"{self.name} hocası {self.branch} dersi veriyor")
 
-ogrenciA=Student("Hüseyin","Tunç",21)
-ogrenciB=Student("Cihan","Dertlioğlu",19)
-ogrenciC=Student("Gizay","Haseken",22)    
-ogrenciD=Student("Enver","Aktaş",21)
-teacher=Teacher("İsmail Hakkı","Tuncel",55)
-
+ogrenciA=Student("Hüseyin","Tunç",21,200)
+ogrenciB=Student("Cihan","Dertlioğlu",19,32)
+ogrenciC=Student("Gizay","Haseken",22,3123)    
+ogrenciD=Student("Enver","Aktaş",21,131)
+teacher=Teacher("İsmail Hakkı","Tuncel",55,"Görsel Programlama")
+ogrenciA.study()
+teacher.teach()
 print(ogrenciA.name)
 print(ogrenciB.surname)
 print(ogrenciC.age)
 ogrenciD.intro()
 teacher.intro()
+ogrenciA.intro()
 
