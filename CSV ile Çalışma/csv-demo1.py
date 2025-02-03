6a436441aa2cdc184bba494ef4fa223cadd2f58f
Hüseyin Tunç <huseyint428@gmail.com>
2025-02-03 14:09:23 +0300

    Uygulama 1-CSV ile Çalışma

---
+++ CSV ile Çalışma/csv-demo1.py
@@ -0,0 +1,19 @@
+#user.csv
+import csv
+
+def add_user(firstname,lastname):
+    with open("user.csv","a",encoding="utf-8") as file:
+        csv_write=csv.writer(file)
+        csv_write.writerow([firstname,lastname])
+def get_users():
+    with open("user.csv",encoding="utf-8") as file:
+        csv_reader=csv.DictReader(file)
+        for p in csv_reader:
+            print(p)
+add_user("Hüseyin","Tunç")
+def find_user(firstname,lastname):
+    with open("user.csv") as file:
+        csv_reader=csv.reader(file)
+        for (index,row) in enumerate(csv_reader):
+            if(row[0]==firstname and row[1]==lastname):
+                print(index)
